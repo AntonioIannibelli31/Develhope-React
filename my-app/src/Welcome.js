313f8ae894ae6b5f2c4283b61151e7cd78386149
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-09-06 13:44:13 +0200

    es10

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -4,8 +4,8 @@ export class Welcome extends React.Component {
   render() {
     return (
       <div>
-        <p>Welcome {this.props.name}!</p>
-        <Age age={67} />
+        {this.props.name === "John" && <p>Welcome {this.props.name}!</p>}
+        <Age age={62} />
       </div>
     );
   }
