2e5f8cdc7c52c435adfda2881c57ab186cb5471b
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-09-14 01:34:40 +0200

    es19

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { Login } from "./Login";
 export class App extends React.Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Login
+          onLogin={() => {
+            console.log("username and password are correct");
+          }}
+        />
+      </div>
+    );
   }
 }
---
+++ my-app/src/Login.js
@@ -0,0 +1,34 @@
+import React from "react";
+export class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+  };
+  handleInputs = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <input type="text" name="username" onChange={this.handleInputs}></input>
+        <input
+          type="password"
+          name="password"
+          onChange={this.handleInputs}
+        ></input>
+        <button
+          disabled={
+            this.state.username === "" && this.state.password === ""
+              ? true
+              : false
+          }
+          onClick={this.props.onLogin}
+        >
+          Login
+        </button>
+      </div>
+    );
+  }
+}
