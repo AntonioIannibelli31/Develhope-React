fad8c3d0477856333179f156a22f09572772ab22
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-09-13 17:26:00 +0200

    es19

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
-import { Hello } from "./Hello";
 export class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <Hello />
-      </div>
-    );
+    return <div className="App"></div>;
   }
 }
