f64803fa1c9c2855ff59b03b956db24169477c8c
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-09-15 04:02:00 +0200

    es22

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -6,10 +6,18 @@ export class Login extends React.Component {
   handleForm = (event) => {
     event.preventDefault();
   };
+  componentDidMount() {
+    this.username.focus();
+  }
   render() {
     return (
       <form onSubmit={this.handleForm} ref={this.newRef}>
-        <input name="username"></input>
+        <input
+          name="username"
+          ref={(input) => {
+            this.username = input;
+          }}
+        ></input>
         <input name="password" type="password"></input>
         <button type="submit" onClick={this.props.onLogin}>
           Login
