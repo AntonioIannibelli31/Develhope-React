9a74f40adc0400017e4c416fbfd7f81ad5f09ec9
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-09-15 16:48:35 +0200

    es21

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,17 +1,31 @@
 import React from "react";
-import { createRef } from "react";
 export class Login extends React.Component {
-  newRef = createRef();
+  state = {
+    disabled: true,
+  };
 
   handleForm = (event) => {
     event.preventDefault();
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+    this.setState({
+      disabled: !username || !password,
+    });
   };
   render() {
     return (
-      <form onSubmit={this.handleForm} ref={this.newRef}>
-        <input name="username"></input>
-        <input name="password" type="password"></input>
-        <button type="submit" onClick={this.props.onLogin}>
+      <form onSubmit={this.handleForm}>
+        <input name="username" onChange={this.handleForm}></input>
+        <input
+          name="password"
+          type="password"
+          onChange={this.handleForm}
+        ></input>
+        <button
+          type="submit"
+          onClick={this.props.onLogin}
+          disabled={this.state.disabled}
+        >
           Login
         </button>
         <button type="reset">Reset</button>
