0cbe7a884a0d0366ae6b994ddcfb13f26fbb5934
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-09-17 18:29:43 +0200

    es29

--- my-app/src/DisplayLanguage.js
+++ my-app/src/DisplayLanguage.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { DisplayContext } from "./DisplayContext";
+let string = {
+  it: {
+    choice: "La lingua scelta è Italiano",
+  },
+  en: {
+    choice: "The language chosen is English",
+  },
+};
 export class DisplayLanguage extends React.Component {
   render() {
-    return;
+    return (
+      <DisplayContext.Consumer>
+        {(language) => {
+          return <h1>{string[language].choice}</h1>;
+        }}
+      </DisplayContext.Consumer>
+    );
   }
 }
