1b8f53538bf1fcc6ed4f7464a63faa5f9ddedfbd
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-09-19 17:08:53 +0200

    es30

--- my-app/src/ClickCounter.js
+++ my-app/src/ClickCounter.js
@@ -1,19 +1,14 @@
-import React from "react";
-export class ClickCounter extends React.Component {
-  state = {
-    counter: 0,
-  };
-  eventCounter = () => {
-    this.setState((state) => {
-      return { counter: state.counter + 1 };
-    });
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={this.eventCounter}>Increment</button>
-        <h1>{this.state.counter}</h1>
-      </div>
-    );
+import React, { useState } from "react";
+export function ClickCounter() {
+  const [counter, setCounter] = useState(0);
+  function eventCounter() {
+    setCounter((counter) => counter + 1);
   }
+
+  return (
+    <div>
+      <button onClick={eventCounter}>Increment</button>
+      <h1>{counter}</h1>
+    </div>
+  );
 }
