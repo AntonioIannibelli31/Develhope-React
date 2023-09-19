b0f2f1326c3262274ad61464e08df6e336de9b25
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-09-19 16:21:49 +0200

    es21 corretto

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,31 +1,14 @@
 import React from "react";
 export class Login extends React.Component {
-  state = {
-    username: "",
-    password: "",
-  };
   handleForm = (event) => {
     event.preventDefault();
-    console.log(event.target.username.value.length);
-    this.setState({
-      [event.target.username.name]: event.target.username.value,
-      [event.target.password.name]: event.target.password.value,
-    });
   };
   render() {
     return (
       <form onSubmit={this.handleForm}>
         <input name="username"></input>
         <input name="password" type="password"></input>
-        <button
-          type="submit"
-          onClick={this.props.onLogin}
-          disabled={
-            this.state.username.length > 1 && this.state.password.length > 1
-              ? false
-              : true
-          }
-        >
+        <button type="submit" onClick={this.props.onLogin}>
           Login
         </button>
         <button type="reset">Reset</button>
