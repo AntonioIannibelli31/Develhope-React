e81d959048bad070db1f6bb0945e57d884008d3e
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-09-23 17:44:31 +0200

    es39

--- my-app/src/GithubUser.js
+++ my-app/src/GithubUser.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { GitHubUserHook } from "./GithubUserHook";
 export function GitHubUser({ username }) {
-  const { user } = GitHubUserHook(username);
+  const { userData, error, fetchGithubUser } = GitHubUserHook(username);
   return (
-    <div>
-      {user && <h1>{user.login}</h1>}
-      {user && <h2>{user.id}</h2>}
-    </div>
+    !error && (
+      <div>
+        <button onClick={fetchGithubUser}>Fetch </button>
+        <h1>{userData.login}</h1>
+        <h2>{userData.id}</h2>
+      </div>
+    )
   );
 }
--- my-app/src/GithubUserHook.js
+++ my-app/src/GithubUserHook.js
@@ -1,14 +1,24 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export function GitHubUserHook(username) {
-  const [user, setUser] = useState(null);
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${username}`)
-      .then((res) => res.json())
-      .then((username) => setUser(username))
-      .catch((err) => console.error(err));
-  }, [username]);
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function fetchGithubUser() {
+    let url = `https://api.github.com/users/${username}`;
+
+    try {
+      let response = await fetch(url);
+      let json = await response.json();
+      setUserData(json);
+    } catch (error) {
+      setError(error);
+      setUserData(null);
+    }
+  }
   return {
-    user: user,
+    userData: userData,
+    error: error,
+    fetchGithubUser: fetchGithubUser,
   };
 }
