8560c9dde51b5cb763c309a3397388bd95f355b9
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-10-05 17:50:23 +0200

    es39

--- my-app/src/GithubUser.js
+++ my-app/src/GithubUser.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { GitHubUserHook } from "./GithubUserHook";
 export function GitHubUser({ username }) {
-  const { user } = GitHubUserHook(username);
+  const { user, err } = GitHubUserHook(username);
   return (
     <div>
+      {err && <h1>Error</h1>}
       {user && <h1>{user.login}</h1>}
       {user && <h2>{user.id}</h2>}
     </div>
--- my-app/src/GithubUserHook.js
+++ my-app/src/GithubUserHook.js
@@ -2,13 +2,18 @@ import { useEffect, useState } from "react";
 
 export function GitHubUserHook(username) {
   const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
   useEffect(() => {
     fetch(`https://api.github.com/users/${username}`)
       .then((res) => res.json())
       .then((username) => setUser(username))
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        setError(err);
+        setUserData(null);
+      });
   }, [username]);
   return {
     user: user,
+    err: err,
   };
 }
